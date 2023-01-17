5410782ddec2a0dd0e746315f3fda82354188ab4
IlyaNikitsin <JamsN87@gmail.com>
2023-01-17 10:59:43 +0300

    Seminar

--- Seminar/Program.cs
+++ Seminar/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+/*
+
+int n = new Random().Next(10, 100); // [от 10 до 99]
+Console.WriteLine($ "Случайное число: {n}");
+int n1 = n / 10 
+int n2 = n % 10;
+if (n1 > n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
+    */
+//Task11
+/*
+int n = new Random().Next(100, 1000); // [от 100 до 1000]
+Console.WriteLine($"Случайное число: {n}");
+int n1 = n / 100 ;
+int n3 = n % 10;
+Console.WriteLine(n1 * 10 + n3);
+*/
+
+//Task12
+//Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+/*
+Console.Write("Введите 1-e число : ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-e число : ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    int m = 0;
+ if ( number1 % number2 == 0 ) 
+ {
+    Console.WriteLine("Кратно");
+ }
+ else 
+ { 
+    m = number1 % number2;
+    Console.WriteLine($"{m}" );
+ }
+*/
